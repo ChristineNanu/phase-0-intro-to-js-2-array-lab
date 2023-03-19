704f5229053dfd2c3e924f6b3f805aefa279c377
mundi <christine.mundi@student.moringaschool.com$ git config --global user.email christine.mundi@student.moringaschool.com>
2023-03-20 01:13:01 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+console.log(cats);
+function destructivelyPrependCat(Kitty) {
+  cats.unshift(Kitty)
+}
+ 
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat(Milo) {
+  cats.shift(Milo);
+}
+function appendCat(Whisky) {
+  
+  return [...cats, Whisky]; // return the new array
+}
+
+
+
+function prependCat(Joe) {
+  return [Joe, ...cats]
+}
+
+
+
+
+
+
+
+
+function removeLastCat() {
+  const newCatsArray = cats.slice(0, -1); // make a copy of the cats array without the last cat object
+  return newCatsArray; // return the new array
+}
+
+function removeFirstCat() {
+  const newCatsArray = cats.slice(1); // make a copy of the cats array without the first cat object
+  return newCatsArray; // return the new array
+}
+
+
+
+
+
+
+
